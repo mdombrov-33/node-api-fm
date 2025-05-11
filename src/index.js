21013cba0bccd15968e9de042365dba94b89fa5c
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-11 18:19:49 +0300

    prisma migrations and models end

--- src/index.js
+++
@@ -1,5 +0,0 @@
-const app = require("./server");
-
-app.listen(3000, () => {
-  console.log("Running on port 3000");
-});
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Hello from express");
+  res.status(200);
+  res.json({ message: "Hello" });
+});
+
+export default app;
