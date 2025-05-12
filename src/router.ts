068261b7f64ec765979dd4e212d61e17afb01f51
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-12 14:06:40 +0300

    route validation end

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { body, check, oneOf, validationResult } from "express-validator";
+import { handleInputsErrors } from "./utils/middlewares";
 
 const router = Router();
 
@@ -6,23 +8,74 @@ const router = Router();
 router.get("/product", (req, res) => {
   res.json({ message: "Hello" });
 });
+
 router.get("/product/:id", () => {});
-router.put("/product/:id", () => {});
-router.post("/product", () => {});
+
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputsErrors,
+  (req, res) => {}
+);
+
+router.post("/product", body("name"), handleInputsErrors, () => {});
+
 router.delete("/product/:id", () => {});
 
 //* UPDATE
 router.get("/update", () => {});
+
 router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
-router.post("/update", () => {});
+
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+
+  //   check("status").isIn(Object.values(UPDATE_STATUS))
+  oneOf(
+    [
+      check("status").equals("IN_PROGRESS"),
+      check("status").equals("SHIPPED"),
+      check("status").equals("DEPRECATED"),
+    ],
+    { message: "Invalid status. Must be IN_PROGRESS, SHIPPED, or DEPRECATED." }
+  ),
+
+  body("version").optional(),
+  body("asset").optional(),
+  () => {}
+);
+
+router.post(
+  "/update",
+  body("title").exists(),
+  body("body").exists().isString(),
+  () => {}
+);
+
 router.delete("/update/:id", () => {});
 
 //* UPDATE POINTS
 router.get("/updatepoint", () => {});
+
 router.get("/updatepoint/:id", () => {});
-router.put("/updatepoint/:id", () => {});
-router.post("/updatepoint", () => {});
+
+router.put(
+  "/updatepoint/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  () => {}
+);
+
+router.post(
+  "/updatepoint",
+  body("name").isString(),
+  body("description").isString(),
+  body("updateId").exists().isString(),
+  () => {}
+);
+
 router.delete("/updatepoint/:id", () => {});
 
 export default router;
